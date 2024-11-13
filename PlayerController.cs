9672bb979212242afceb539c43b1d23fe9eabb8f
Alexandra Revan <114621270+RevanRiot@users.noreply.github.com>
2024-11-12 22:32:29 -0600

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,17 @@
+// File: PlayerController.cs
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
